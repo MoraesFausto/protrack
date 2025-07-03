db84af54bbcfe9247733a2d1d86594686618c9f4
Matheus <mvos.developer@gmail.com>
2025-07-03 15:29:59 -0300

    feat: implementa serviço e controlador de tarefas com operações CRUD

---
+++ src/main/java/br/com/edu/alunos/utfpr/protrack/domain/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package br.com.edu.alunos.utfpr.protrack.domain.enums;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/main/java/br/com/edu/alunos/utfpr/protrack/infrastructure/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.edu.alunos.utfpr.protrack.infrastructure.repositories;
+
+import br.com.edu.alunos.utfpr.protrack.domain.models.Task; // adicione esta linha
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProjectId(Long projectId);
+}
